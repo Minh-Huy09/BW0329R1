aa8d9aad296cc1093c633c0b50c197fe536728c4
lehuy.01632449327@gmail.com <lehuy.01632449327@gmail.com>
2024-08-10 19:04:38 +0700

    react router

--- C0324M3/NodeJS/demo/src/App.js
+++ C0324M3/NodeJS/demo/src/App.js
@@ -1,33 +1,34 @@
-// import logo from './logo.svg';
-import './App.css';
-import axios from "axios";
-import HienMang from "./component/HienMang";
-import TinhToan from "./component/TinhToan";
-import Product from "./component/Product";
-import TestLifeCycle from "./component/test_life_cycle";
-import {FComponent} from "./component/FComponent";
-import FProduct from './component/FProduct';
-import Covid from './component/Covid';
-import Calculator from './component/Calculator';
-import TestFuncComponent  from './component/TestFuncComponent';
-import Country from './component/Country';
 
+import './App.css';
+import Calculator from "./components/Calculator";
+import HienMang from "./components/HienMang";
+import Product from "./components/Product";
+import PersonList from "./components/PersonList";
+import Covid from "./components/Covid";
+import {TestFuncComponent} from "./components/TestFuncComponent";
+import FProduct from "./components/FProduct";
+import Country from "./components/Country";
+import Student from "./components/Student";
+import {Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
 function App() {
-  return (
-    <>
-      {/*<ClassSample />*/}
-      {/*<HienMang />*/}
-      {/*<TinhToan />*/}
-      {/*  <Product />*/}
-      {/* <TestLifeCycle/> */}
-      {/* <FComponent/> */}
-      {/* <Covid/> */}
-      {/* <Calculator/> */}
-      {/* <TestFuncComponent/> */}
-      <Country/>
-      {/* <FProduct/> */}
-    </>
-  );
+	return (
+		<>
+			<Navbar/>
+			<Routes>
+				<Route path={'/student'} element={<Student/>}/>
+				<Route path={'/country'} element={<Country/>}/>
+				<Route path={'/fproduct'} element={<FProduct/>}/>
+				<Route path={'/testfunccomponent'} element={<TestFuncComponent/>}/>
+				<Route path={'/covid'} element={<Covid/>}/>
+				<Route path={'/product'} element={<Product/>}/>
+				<Route path={'/calculator'} element={<Calculator/>}/>
+				<Route path={'/personlist'} element={<PersonList/>}/>
+				<Route path={'/hienmang'} element={<HienMang/>}/>
+        <Route path={'/navbar'} element={<Navbar/>}/> 
+			</Routes>
+		</>
+	);
 }
 
 export default App;
---
+++ C0324M3/NodeJS/demo2/src/App.js
@@ -0,0 +1,63 @@
+// 1. PAGE//
+
+// import './App.css';
+// import {Link, Route, Routes} from "react-router-dom";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Home from './pages/Home';
+// import Product from "./pages/Product";
+// import Orders from "./pages/Orders";
+
+
+
+// function App() {
+//     return (
+//         <>
+//             <Routes>
+//                 <Route path={''} element={<Login/>}></Route>
+//                 <Route path={'home'} element={<Home/>}>
+//                     <Route path={''} element={<Product/>}/>
+//                     <Route path={'orders'} element={<Orders/>}/>
+//                 </Route>
+//                 <Route path={'register'} element={<Register/>}></Route>
+
+//             </Routes>
+//         </>
+//     );
+// }
+
+// export default App;
+
+
+// 2. CG_react_router_3
+
+// import './App.css';
+// import {Route, Routes} from "react-router-dom";
+// import Home from "./CG_react_router_3/Home";
+// import Cart from "./CG_react_router_3/Cart";
+// import DetailProduct from "./CG_react_router_3/DetailProduct";
+// import Login from "./CG_react_router_3/Login";
+// import Register from "./CG_react_router_3/Register";
+// import HomeContent from "./CG_react_router_3/HomeContent";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<Home/>}>
+//           <Route path="/" element={<HomeContent/>}/>
+//           <Route path="/cart" element={<Cart/>}/>
+//           <Route path="/detail" element={<DetailProduct/>}/>
+//           <Route path="/login" element={<Login/>}/>
+//           <Route path="/register" element={<Register/>}/>
+//           </Route>
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
